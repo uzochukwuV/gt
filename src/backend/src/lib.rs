ad396d5c32f568668066a44fcbcd88928223f690
Antoine Espieux <antoine.espieux@hotmail.fr>
2025-05-15 19:49:05 +0200

    add llm backend

--- src/backend/src/lib.rs
+++ src/backend/src/lib.rs
@@ -1,6 +1,25 @@
 use ic_cdk::export_candid;
 use std::cell::RefCell;
 
+use ic_llm::{ChatMessage, Model};
+
+#[ic_cdk::update]
+async fn prompt(prompt_str: String) -> String {
+    ic_llm::prompt(Model::Llama3_1_8B, prompt_str).await
+}
+
+#[ic_cdk::update]
+async fn chat(messages: Vec<ChatMessage>) -> String {
+    let response = ic_llm::chat(Model::Llama3_1_8B)
+        .with_messages(messages)
+        .send()
+        .await;
+
+    // A response can contain tool calls, but we're not calling tools in this project,
+    // so we can return the response message directly.
+    response.message.content.unwrap_or_default()
+}
+
 thread_local! {
     static COUNTER: RefCell<u64> = const { RefCell::new(0) };
 }
