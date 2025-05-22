08150e9b73d8d2799c2a29c1e0a64c431e464a9f
tiago89 <19423709+tiagoicp@users.noreply.github.com>
2025-05-22 11:18:58 +0000

    added tailwindcss

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import environment from "vite-plugin-environment";
 import dotenv from "dotenv";
+import tailwindcss from "@tailwindcss/vite";
 
 dotenv.config();
 
@@ -30,6 +31,7 @@ export default defineConfig({
   },
   plugins: [
     react(),
+    tailwindcss(),
     environment("all", { prefix: "CANISTER_" }),
     environment("all", { prefix: "DFX_" }),
   ],
