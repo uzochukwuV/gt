1a5e3a4526862f51d35a2f5ced3b316d467b0f94
tiago89 <19423709+tiagoicp@users.noreply.github.com>
2025-05-24 08:06:14 +0000

    added husky to format before commit

--- src/frontend/src/components/Button.tsx
+++ src/frontend/src/components/Button.tsx
@@ -20,7 +20,7 @@ export function Button({
     <button
       onClick={onClick}
       disabled={disabled}
-      className={`font-inherit focus:outline-auto mx-2 cursor-pointer rounded-lg border border-gray-500 bg-gray px-5 py-3 text-base font-medium text-white transition-colors duration-200 hover:border-blue-400 focus:outline-4 focus:outline-blue-400 disabled:cursor-not-allowed disabled:opacity-50 ${className} `.trim()}
+      className={`font-inherit focus:outline-auto bg-gray mx-2 cursor-pointer rounded-lg border border-gray-500 px-5 py-3 text-base font-medium text-white transition-colors duration-200 hover:border-blue-400 focus:outline-4 focus:outline-blue-400 disabled:cursor-not-allowed disabled:opacity-50 ${className} `.trim()}
     >
       {children}
     </button>
